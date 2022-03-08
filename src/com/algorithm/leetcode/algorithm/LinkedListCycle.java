5a99af64a2204b94d96e73e52895a2f068671e93
rinjyu <rinjyu@naver.com>
2022-03-08 22:15:35 +0900

    [20220308] LeetCode Algorithm - Linked List Cycle

---
+++ src/com/algorithm/leetcode/algorithm/LinkedListCycle.java
@@ -0,0 +1,34 @@
+package com.algorithm.leetcode.algorithm;
+
+public class LinkedListCycle {
+
+    public static class ListNode {
+
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    public static boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode slowNode = head;
+        ListNode fastNode = head;
+        while (fastNode.next != null && fastNode.next.next != null) {
+            slowNode = slowNode.next;
+            fastNode = fastNode.next.next;
+            if (slowNode == fastNode) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
