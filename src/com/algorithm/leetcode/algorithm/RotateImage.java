83d607874eb91881eb1b3fa607f26e7737d5c72f
rinjyu <rinjyu@naver.com>
2022-03-15 22:24:53 +0900

    [20220315] LeetCode Algorithm - Rotate Image

---
+++ src/com/algorithm/leetcode/algorithm/RotateImage.java
@@ -0,0 +1,37 @@
+package com.algorithm.leetcode.algorithm;
+
+import java.util.Arrays;
+
+public class RotateImage {
+
+    public static void main(String[] args) {
+        int[][] matrix = {{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
+        rotate(matrix);
+        System.out.println(Arrays.deepToString(matrix));
+    }
+
+    private static void rotate(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[0].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int[] n : matrix) {
+            reverse(n, n.length - 1);
+        }
+    }
+
+    private static void reverse(int[] array, int end) {
+        int start = 0;
+        while (start <= end) {
+            int temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
