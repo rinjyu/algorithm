fe32a3c9c9bc0cc4d511379dcc8bdc9b5ac57b2a
rinjyu <rinjyu@naver.com>
2022-06-15 22:11:53 +0900

    [20220615] LeetCode Algorithm - Move Zeroes

---
+++ src/com/algorithm/leetcode/algorithm/MoveZeroes.java
@@ -0,0 +1,27 @@
+package com.algorithm.leetcode.algorithm;
+
+import java.util.Arrays;
+
+public class MoveZeroes {
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 3, 12};
+        moveZeroes(nums);
+    }
+
+    public static void moveZeroes(int[] nums) {
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                swap(nums, i, index++);
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
